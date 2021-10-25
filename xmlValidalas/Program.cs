c62a9a65f0044798e832fffb7e2d9a642ba3b113
borossandor27 <boros.sanyi@gmail.com>
2021-10-25 21:10:20 +0200

    Add project files.

---
+++ xmlValidalas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace xmlValidalas
+{
+    class Program
+    {
+        static void Main()
+        {
+            XmlReaderSettings booksSettings = new XmlReaderSettings();
+            booksSettings.Schemas.Add("http://www.contoso.com/books", "books.xsd");
+            booksSettings.ValidationType = ValidationType.Schema;
+            booksSettings.ValidationEventHandler += new ValidationEventHandler(booksSettingsValidationEventHandler);
+
+            XmlReader books = XmlReader.Create("books.xml", booksSettings);
+
+            while (books.Read()) { }
+        }
+
+        static void booksSettingsValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Warning)
+            {
+                Console.Write("WARNING: ");
+                Console.WriteLine(e.Message);
+            }
+            else if (e.Severity == XmlSeverityType.Error)
+            {
+                Console.Write("ERROR: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
